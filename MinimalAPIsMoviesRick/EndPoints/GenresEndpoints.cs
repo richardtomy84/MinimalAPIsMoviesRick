2f57edde0d5f4432687e245e5211498f656ccc25
Richard Tomy <richardtomy84@outlook.com>
2024-08-06 00:26:28 +0530

    DTO Module added - Data Transfer objct , working concept

---
+++ MinimalAPIsMoviesRick/EndPoints/GenresEndpoints.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OutputCaching;
+using MinimalAPIsMoviesRick.Entities;
+using MinimalAPIsMoviesRick.Repositories;
+
+namespace MinimalAPIsMoviesRick.EndPoints
+{
+    public static class GenresEndpoints
+    {
+        public static RouteGroupBuilder MapGenres(this RouteGroupBuilder group)
+        {
+            group.MapGet("", GetGenre).CacheOutput(c => c.Expire(TimeSpan.FromSeconds(60)).Tag("genres-get"));
+
+            group.MapGet("/{id:int}", GetById);
+
+            //Data from SQL
+            group.MapPost("/", Create);
+
+            group.MapPut("/{id:int}", Update);
+
+            group.MapDelete("/{id:int}", Delete);
+
+            return group;
+        }
+
+
+
+        //Lamda Expressions to Named Methods
+        static async Task<Ok<List<Genre>>> GetGenre(IGenresRepository repository)
+        {
+            var geners = await repository.GetAll();
+            return TypedResults.Ok(geners);
+        }
+
+        static async Task<Results<Ok<Genre>, NotFound>> GetById(int id, IGenresRepository genresRepository)
+        {
+
+            var genre = await genresRepository.GetById(id);
+
+            if (genre is null)
+            {
+                return TypedResults.NotFound();
+
+                // return Results.NotFound();
+            }
+            // return Results.Ok(genre);
+            return TypedResults.Ok(genre);
+
+        }
+
+        static async Task<Created<Genre>> Create(Genre genre, IGenresRepository genresRepository, IOutputCacheStore outputCacheStore)
+        {
+            var id = await genresRepository.Create(genre);
+            await outputCacheStore.EvictByTagAsync("genres-get", default);
+            // return TypedResults.Ok(genre);
+            return TypedResults.Created($"/genres/{id}", genre);
+
+
+        }
+
+
+        static async Task<Results<NotFound, NoContent>> Update(int id, Genre genre, IGenresRepository repository, IOutputCacheStore outputCacheStore)
+        {
+
+            var exists = await repository.Exists(id);
+
+            if (!exists)
+            {
+                return TypedResults.NotFound();
+
+            }
+
+            await repository.Update(genre);
+            await outputCacheStore.EvictByTagAsync("genres-get", default);
+            return TypedResults.NoContent();
+        }
+
+        static async Task<Results<NotFound, NoContent>> Delete(int id, IGenresRepository repository, IOutputCacheStore outputCacheStore)
+        {
+            var exists = await repository.Exists(id);
+
+            if (!exists)
+            {
+                return TypedResults.NotFound();
+
+            }
+
+            await repository.Delete(id);
+            await outputCacheStore.EvictByTagAsync("genres-get", default);
+            return TypedResults.NoContent();
+        }
+
+        //Lamda Expressions to Named Methods - End
+
+    }
+}
--- MinimalAPIsMoviesRick/Program.cs
+++ MinimalAPIsMoviesRick/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.Extensions.Options;
+using MinimalAPIsMoviesRick.EndPoints;
 using MinimalAPIsMoviesRick.Entities;
 using MinimalAPIsMoviesRick.Repositories;
 
@@ -44,7 +45,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-var genresEndpoints = app.MapGroup("/genres");
+ app.MapGroup("/genres").MapGenres();  // Removed var genresEndpoints =
 
 
 
@@ -96,16 +97,7 @@ app.MapGet("/genres", () =>
 
 */
 
-genresEndpoints.MapGet("", GetGenre).CacheOutput(c => c.Expire(TimeSpan.FromSeconds(60)).Tag("genres-get"));
 
-genresEndpoints.MapGet("/{id:int}", GetById);
-
-//Data from SQL
-genresEndpoints.MapPost("/", Create);
-
-genresEndpoints.MapPut("/{id:int}", Update);
-
-genresEndpoints.MapDelete("/{id:int}", Delete);
 //Middlewares Zone - End
 
 //Endpoints End
@@ -113,66 +105,3 @@ genresEndpoints.MapDelete("/{id:int}", Delete);
 app.Run();
 
 
-//Lamda Expressions to Named Methods
-static async Task<Ok<List<Genre>>> GetGenre(IGenresRepository repository) 
-{   
-    var geners = await repository.GetAll();
-    return TypedResults.Ok(geners);
-}
-
-static async Task<Results<Ok<Genre>,NotFound>> GetById (int id, IGenresRepository genresRepository) {
-
-    var genre = await genresRepository.GetById(id);
-
-    if (genre is null)
-    {
-         return TypedResults.NotFound();
-
-        // return Results.NotFound();
-    }
-    // return Results.Ok(genre);
-    return TypedResults.Ok(genre);
-
-}
-
-static async Task<Created<Genre>> Create (Genre genre, IGenresRepository genresRepository, IOutputCacheStore outputCacheStore) {
-    var id = await genresRepository.Create(genre);
-    await outputCacheStore.EvictByTagAsync("genres-get", default);
-    // return TypedResults.Ok(genre);
-    return TypedResults.Created($"/genres/{id}", genre);
-
-
-}
-
-
-static async Task<Results<NotFound,NoContent>>  Update (int id, Genre genre, IGenresRepository repository, IOutputCacheStore outputCacheStore)  {
-
-    var exists = await repository.Exists(id);
-
-    if (!exists)
-    {
-        return TypedResults.NotFound();
-
-    }
-
-    await repository.Update(genre);
-    await outputCacheStore.EvictByTagAsync("genres-get", default);
-    return TypedResults.NoContent();
-}
-
-static async Task<Results<NotFound,NoContent>> Delete (int id, IGenresRepository repository, IOutputCacheStore outputCacheStore)
-{
-    var exists = await repository.Exists(id);
-
-    if (!exists)
-    {
-        return TypedResults.NotFound();
-
-    }
-
-    await repository.Delete(id);
-    await outputCacheStore.EvictByTagAsync("genres-get", default);
-    return TypedResults.NoContent();
-}
-
-//Lamda Expressions to Named Methods - End
