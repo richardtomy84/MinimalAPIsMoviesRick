6faefe1125bce344775bf8c5fbe53872e7591c3d
Richard Tomy <richardtomy84@outlook.com>
2024-08-26 23:53:16 +0530

    customize validation message end

--- MinimalAPIsMoviesRick/Validaton/CreateGenereDTOValidator.cs
+++ MinimalAPIsMoviesRick/Validaton/CreateGenereDTOValidator.cs
@@ -7,7 +7,21 @@ namespace MinimalAPIsMoviesRick.Validaton
     {
         public CreateGenereDTOValidator()
         {
-            RuleFor(p=>p.Name).NotEmpty().WithMessage("The field {PropertyName} is required");
+            RuleFor(p=>p.Name).NotEmpty().WithMessage("The field {PropertyName} is required")
+                .MaximumLength(150)
+                .WithMessage("The Field {PropertyName} should be less than {MaxLength} characters")
+                .Must(FirstLetterIsUppercase).WithMessage("The field {PropertyName} should start with upercase");
+        }
+
+        private bool FirstLetterIsUppercase(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return true;
+            }
+
+            var firstLetter = value[0].ToString();
+            return firstLetter == firstLetter.ToUpper();
         }
     }
 }
